6cf2474d70fd77fdfc6bb541e61cac6b9dfca919
Alexander Clare <alexander.clare@gmail.com>
2012-12-31 00:41:39 -0500

    Forum page

--- august.js
+++ august.js
@@ -1,22 +1,35 @@
 // TODO
 //   - remove clutter
+//   - make variables/selector names unique to follow logic easier
 //   - separate client and server code
 //   - actual security
 //   - persistent session information in localStorage
 
 Users = new Meteor.Collection('users');
-//Threads = new Meteor.Collection('threads');
+Threads = new Meteor.Collection('threads');
 
 function login(input) {
   var user = String(input || '');
   if (user) {
-    if (Users.findOne({username:user})) {
-      Session.set('username', user);
-    } else {
+    if (!Users.findOne({username:user})) {
       Users.insert({username:user,
-                    timestamp:(new Date).getTime()});
-      Session.set('username', user);
+                    timestamp:Date.now()});
     }
+    Session.set('username', user);
+  }
+}
+
+function startThread(input) {
+  var topic = String(input || '');
+  if (topic) {
+    if (!Threads.findOne({topic:topic})) {
+      var now = new Date(Date.now());
+      // TODO Use lib/underscore.string.sprintf
+      Threads.insert({topic:topic,
+                      timestamp:now,
+                      timestring:now.toUTCString()});
+    }
+    Session.set('thread', topic);
   }
 }
 
@@ -32,10 +45,34 @@ if (Meteor.isClient) {
     }
   });
 
-  // TODO Remember to add "click on top nav -> return to default page" here
   Template.top.events({
+    'click .brand': function(event) {
+      Session.set('thread', '')
+    },
     'click #logout': function(event) {
       Session.set('username', '');
+    }
+  });
+
+  Template.forum.events({
+    'click .thread': function(event) {
+      startThread(event.target.text);
+    },
+    'click .add-on': function(event) {
+      startThread(this.topic.value);
+    },
+    'keyup #topic': function(event) {
+      if (event.which === 13) {
+        startThread(this.topic.value)
+      };
+    }
+  });
+
+  Template.forum.helpers({
+    'threads': function() {
+      return Threads.find(
+        {}, {fields:{topic:1, timestring:1},
+             sort:{timestamp: -1}});
     }
   });
 
